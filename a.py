2f452ef303bf6da11e7c3f1cc0f2481611702039
Faheem <farooqui.faheem033@gmail.com>
2025-01-14 14:53:53 +0530

    array code

--- a.py
+++ a.py
@@ -6,6 +6,7 @@ print(car1,car2,car3)
 
 import numpy as np
 
+
 # create an array
 
 arr=np.array([1,2,3,4,5])
@@ -13,3 +14,20 @@ arr=np.array([1,2,3,4,5])
 # perform operation
 print("Original Array",arr)
 print("Array squared",arr**2)
+
+print(arr)
+print(arr[1])
+
+import array
+
+arr=array.array("i",[1,2,3,4,5])
+
+print(arr)
+
+print(arr[0])
+print(arr[-1])
+
+print(arr[:-1])
+
+
+
