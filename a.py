c19d73cc0b9b73002f44ad5681b2e7e580421569
Faheem <farooqui.faheem033@gmail.com>
2025-01-14 13:05:17 +0530

    code

--- a.py
+++ a.py
@@ -3,3 +3,13 @@ car2="Ford"
 car3="BMW"
 
 print(car1,car2,car3)
+
+import numpy as np
+
+# create an array
+
+arr=np.array([1,2,3,4,5])
+
+# perform operation
+print("Original Array",arr)
+print("Array squared",arr**2)
--- pand.py
+++ pand.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import difflib
+
+
 
 df=pd.read_csv("/home/faheem/Desktop/python_zero_to_hero/testdata.csv")
 
@@ -19,3 +22,17 @@ print(df["Age"])
 print(df["email"])
 print(df["Age"])
 print(df["captcha"])
+
+print(df.columns)
+
+print(df.head())
+print(df.sample())
+
+df=pd.read_csv("/home/faheem/Desktop/python_zero_to_hero/testdata.csv")
+print(df)
+
+close_matches=difflib.get_close_matches("Age",df.columns)
+
+print(close_matches)
+
+
