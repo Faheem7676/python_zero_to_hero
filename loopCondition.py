76af192f649c858d38e143f5853f5e5fbc4e6d3e
Faheem <farooqui.faheem033@gmail.com>
2025-01-15 21:11:54 +0530

    updated

---
+++ loopCondition.py
@@ -0,0 +1,16 @@
+count=0
+
+while count<5:
+    
+    print(count)
+    
+    count=count+1
+    
+else:
+    print(count)
+    
+    # print from 0 to 4 
+    
+
+
+print("I hope you are good and ready to move from there.\n Are you?")
