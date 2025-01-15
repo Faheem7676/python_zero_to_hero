8775202ed5de84316f027f9a570d6875872aa826
Faheem <farooqui.faheem033@gmail.com>
2025-01-15 10:37:24 +0530

    tcode

---
+++ prepration.py
@@ -0,0 +1,29 @@
+
+# data type
+# str, 
+# int,float,complex
+# list,tuple,set,range
+# dict,
+# bool
+# bytes,bytearray,memoryview
+# NoneType
+
+x=4
+print(type(x))
+str1="Faheem"
+print(type(str))
+l=[2,3,4,4]
+print(type(l))
+tuple=(1,3,5)
+print(type(tuple))
+set={4,3,"Faheem"}
+print(type(set))
+
+dict={1: "value1",2: "value2"}
+
+print(type(dict))
+
+print(dict[1])
+
+x=str("Hello world")
+print(x)
