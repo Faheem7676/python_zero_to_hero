1562dfc2f8a5e309ed23bd4ec370a38456344041
Faheem <farooqui.faheem033@gmail.com>
2025-01-15 18:41:26 +0530

    main

---
+++ conditional.py
@@ -0,0 +1,27 @@
+a=4
+
+if a>0:
+    print("A is greater than zero")
+    
+    
+a=3
+
+if a<0:
+    
+    print("A is greater than zero")
+    
+else:
+    print("A is not greater than zero")
+    
+    
+a=-1
+
+if a>0:
+    
+    print("A is greater than zero")
+    
+elif a<0:
+    print("A is less than zero")
+    
+else:
+    print("A is zero")
