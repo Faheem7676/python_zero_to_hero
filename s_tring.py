9c6f1a18bd2fc9de6eab2b718700dd1d3e5991f3
Faheem <farooqui.faheem033@gmail.com>
2025-01-17 20:04:19 +0530

    updated

--- s_tring.py
+++ s_tring.py
@@ -8,3 +8,39 @@ greeting="Hello World"
 
 print(greeting)
     
+    
+print(len(greeting))
+
+sentence="I hope you are enjoying python code of 30 days"
+
+print(sentence)
+
+multiline_string='''
+This is sentence where.\n we can go and enjoy
+there is python code program I can give you new
+line '''
+
+print(multiline_string)
+
+string_multiline="""
+this is the code where we can get all code"""
+
+print(string_multiline)
+
+first_name="Rahul"
+last_name="Bosssss"
+
+full_name=first_name+last_name
+print(full_name)
+
+print(len(first_name))
+print(len(last_name))
+print(len(first_name)>(len(last_name)))
+
+print(len(full_name))
+
+print("I am not goin to tell you. \n You get it from your self.")
+
+print("Days\tTopics\tExercise")
+
+print("Day t1\t5\t6")
