a2c771f16a68b1991d0f377629cb7d4d8be1e9d4
Faheem <farooqui.faheem033@gmail.com>
2025-01-27 12:59:18 +0530

    code update

---
+++ module.py
@@ -0,0 +1,23 @@
+from math import pi, sqrt, pow, floor, ceil, log10
+import math
+import string
+from random import random,randint
+
+print(pi)
+print(sqrt (2))
+print(pow,(2,3))
+print(floor (9.8))
+print(ceil(9.81))
+print(math.log10(100))
+print(pi)                 # 3.141592653589793
+print(sqrt(2))            # 1.4142135623730951
+print(pow(2, 3))          # 8.0
+print(floor(9.81))        # 9
+print(ceil(9.81))         # 10
+print(math.log10(100))    # 2
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+print(random())
+print(randint(5,10))
